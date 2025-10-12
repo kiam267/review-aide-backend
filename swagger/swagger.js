8022d211d20740c22abecf3158173b688a745396
Abid Hasan Kiam <kiamhasan267@gmail.com>
2025-10-12 16:39:18 +0600

    swagger problem try to fix

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,6 +1,7 @@
 // swagger.js
 import swaggerJSDoc from 'swagger-jsdoc';
 import swagger from 'swagger-ui-express';
+import express from 'express';
 
 const CSS_URL =
   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
@@ -30,12 +31,7 @@ const setupSwagger = app => {
   app.use(
     '/api/v2/docs',
     swagger.serve,
-    swagger.setup(swaggerSpec, {
-      customCss:
-        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-      customCssUrl: CSS_URL,
-    })
-    // express.static(swaggerUiPath)
+    swagger.setup(swaggerSpec)
   );
 };
 
